7b9a9ba3f9a3a85bc4a50b278fa57fe192820bc3
franjramirez <99449519+franjramirez@users.noreply.github.com>
2022-02-15 19:04:21 -0500

    Update helperRemove.py

--- wordle/helperRemove.py
+++ wordle/helperRemove.py
@@ -1,8 +1,10 @@
+#The main function that removes the words
 def helpRem(results, guess, words):
     orange = ''
     green = ''
     greenIndex = []
     index = 0
+    #First check for the green letters in order to avoid removing words that could be valid (In the case that a letter has a "g" and a "n" in a word)
     for check in results:
         if(check == "g"):
             words = remGreen(guess, words,index) 
@@ -10,6 +12,7 @@ def helpRem(results, guess, words):
             greenIndex.append(index)
         index += 1
 
+    #Second check for the orange letters in order to avoid removing words that could be valid (In the case that a letter has a "g" and a "o" in a word)
     index = 0
     for check in results:
         if check == "o":
@@ -17,16 +20,16 @@ def helpRem(results, guess, words):
             orange += guess[int(index)]
         index += 1
     
+    #Remove the grey letters keeping in mind the green and oranges
     index = 0
     for check in results:
         if check == "n":
             words = remNonHelp(guess, words,index,orange, green, greenIndex)
         index += 1
-
     return words
 
+#Removes the grey letters
 def remNonHelp(guess, words, index, orange, green, greenIndex):
-    
     if (guess[index] not in orange) & (guess[index] not in green):
         newList = []
         newList = [word for word in words if (guess[index] not in word)]
@@ -39,11 +42,12 @@ def remNonHelp(guess, words, index, orange, green, greenIndex):
     return newList
 
     
-
+#Removes the green letters
 def remGreen(guess, words, index):
     words = [x for x in words if guess[index] is x[index]]
     return words
 
+#Removes the orange letters
 def remOra(guess, words, index):
     newList = []
     for word in words:
